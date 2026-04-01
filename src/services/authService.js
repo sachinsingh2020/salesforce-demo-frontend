e345648d748150619d51b82b0b3d9f2010aed8a8
sachinsingh2020 <sachinthegreat1001@gmail.com>
2026-04-01 09:49:54 +0530

    consoling 2

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,11 +1,17 @@
 export const getLoginUrl = () => {
   console.log("client id: ", import.meta.env.VITE_CLIENT_ID);
   console.log("redirect uri: ", import.meta.env.VITE_REDIRECT_URI);
+  console.log("auth url: ", import.meta.env.VITE_AUTH_URL);
   const params = new URLSearchParams({
     response_type: "code",
     client_id: import.meta.env.VITE_CLIENT_ID,
     redirect_uri: import.meta.env.VITE_REDIRECT_URI,
   });
+
+  console.log(
+    "total url returning: ",
+    `${import.meta.env.VITE_AUTH_URL}?${params.toString()}`,
+  );
 
   return `${import.meta.env.VITE_AUTH_URL}?${params.toString()}`;
 };
